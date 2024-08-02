12d24b46b55582da999eba3b47264f4f1725dfb9
soner3 <sonerastan@icloud.com>
2024-08-02 23:20:23 +0200

    Started Docker Compose

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv("../../env/backend.env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +79,12 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("NAME"),
+        "USER": os.environ.get("USER"),
+        "PASSWORD": os.environ.get("PASSWORD"),
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="text-3xl font-bold text-blue-500">
-      Hello Learning System
+      Hello Learning System From Docker
     </div>
   );
 }
